e3f46b58020d14990baf134390159b802135dd2d
Alisa Wallace <awallace@mitre.org>
2024-09-17 14:44:31 -0700

    Update test kit/suite description

--- lib/smart_udap_harmonization_test_kit.rb
+++ lib/smart_udap_harmonization_test_kit.rb
@@ -8,8 +8,9 @@ module SMARTUDAPHarmonizationTestKit
     id :smart_udap_harmonization
     title 'SMART-UDAP Harmonization Test Suite'
     description %(
-      This suite is an initial investigation into requirements for a server adhering to both SMART App Launch STU2.0 and
-      UDAP Security STU1.0 standards.
+      This test kit tests server support for the [HL7 UDAP STU1.0 IG](https://hl7.org/fhir/us/udap-security/STU1/index.html) set of UDAP workflows (discovery, client registration, and authentication/authorization) using SMART App Launch scopes ([STU1](https://hl7.org/fhir/smart-app-launch/1.0.0/scopes-and-launch-context/index.html) or [STU2](https://hl7.org/fhir/smart-app-launch/STU2.2/scopes-and-launch-context.html)).
+
+      Conformant systems are expected to comply with all UDAP requirements, with the exception of requirements pertaining to scopes, where the system is expected to comply with SMART App Launch scopes requirements.  No other requirements from the SMART App Launch framework are assessed. 
     )
 
     # These inputs will be available to all tests in this suite
