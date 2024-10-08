fb25d044c114284d89c8183163d6cd4e18d32b8e
Alisa Wallace <140003092+alisawallace@users.noreply.github.com>
2024-10-08 08:49:48 -0700

    Update suite ID to not include "test kit"
    Co-authored-by: Stephen MacVicar <Jammjammjamm@users.noreply.github.com>

--- lib/smart_udap_harmonization_test_kit.rb
+++ lib/smart_udap_harmonization_test_kit.rb
@@ -3,7 +3,7 @@ require_relative 'smart_udap_harmonization_test_kit/harmonization_authorization_
 
 module SMARTUDAPHarmonizationTestKit
   class Suite < Inferno::TestSuite
-    id :smart_udap_harmonization_test_kit
+    id :smart_udap_harmonization
     title 'SMART-UDAP Harmonization'
     description %(
       This test kit tests server support for the [HL7 UDAP STU1.0 IG](https://hl7.org/fhir/us/udap-security/STU1/index.html) set of UDAP workflows (discovery, client registration, and authentication/authorization) using [SMART App Launch STU2 scopes](https://hl7.org/fhir/smart-app-launch/STU2.2/scopes-and-launch-context.html).
