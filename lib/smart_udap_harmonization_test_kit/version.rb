86dc2b76874e02aa794bd3d40155e7f84fe1f47b
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2025-03-06 12:20:18 -0500

    Release 0.10.1 (#12)
    * update required udap test kit version
    
    * bump version

--- lib/smart_udap_harmonization_test_kit/version.rb
+++ lib/smart_udap_harmonization_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module SMART_UDAP_HarmonizationTestKit
-  VERSION = '0.10.0'
-  LAST_UPDATED = '2026-02-27'
+  VERSION = '0.10.1'
+  LAST_UPDATED = '2025-03-06'
 end
