6302aa9a35546a7b20685fdf343b54b46610ac8a
Vanessa Fotso <vfotso@mitre.org>
2025-03-26 11:21:17 -0400

    bump version
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/smart_udap_harmonization_test_kit/version.rb
+++ lib/smart_udap_harmonization_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module SMART_UDAP_HarmonizationTestKit
-  VERSION = '0.10.2'
-  LAST_UPDATED = '2025-03-06'
+  VERSION = '0.11.0'
+  LAST_UPDATED = '2025-03-26'
 end
