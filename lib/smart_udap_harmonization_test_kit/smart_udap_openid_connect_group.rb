9a8679eb44cde7243186f5a1d72bafa695dc976a
Vanessa Fotso <46642178+vanessuniq@users.noreply.github.com>
2025-03-26 11:17:36 -0400

    FI-3746:  Update test kit to use authinfo (#14)
    * bump dependencies
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * update service config
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * updated the smart app portion to use auth info
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * remove setup test
    
    * fix linting
    
    * remove unneeded configuration
    
    * add registration scope input to token response scope test
    
    * fix specs and linting
    
    ---------
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    Co-authored-by: Stephen MacVicar <smacvicar@mitre.org>

--- lib/smart_udap_harmonization_test_kit/smart_udap_openid_connect_group.rb
+++ lib/smart_udap_harmonization_test_kit/smart_udap_openid_connect_group.rb
@@ -12,20 +12,14 @@ module SMART_UDAP_HarmonizationTestKit
 
     config(
       inputs: {
-        client_id: {
-          name: :udap_client_id,
-          title: 'UDAP Client ID'
+        smart_auth_info: {
+          title: 'UDAP Auth Info',
+          options: {
+            mode: 'access'
+          }
         },
         token_response_body: {
           name: :udap_auth_code_flow_token_exchange_response_body
-        },
-        token_retrieval_time: {
-          name: :udap_auth_code_flow_token_retrieval_time
-        },
-        requested_scopes: {
-          name: :udap_auth_code_flow_registration_scope,
-          title: 'Requested Scopes',
-          description: 'Scopes client requested from the authorization server during the authorization step.'
         },
         url: {
           name: :udap_fhir_base_url,
@@ -33,37 +27,5 @@ module SMART_UDAP_HarmonizationTestKit
         }
       }
     )
-
-    test do
-      id :smart_udap_openid_connect_setup
-      title 'OpenID Connect Test Setup'
-
-      input :token_response_body,
-            title: 'Token Exchange Response Body',
-            description: 'JSON response body returned by the authorization server during the token exchange step'
-
-      input :token_retrieval_time,
-            title: 'Token Retrieval Time'
-
-      output :id_token,
-             :access_token,
-             :smart_credentials
-
-      run do
-        assert_valid_json(token_response_body)
-
-        token_response = JSON.parse(token_response_body)
-
-        output id_token: token_response['id_token'],
-               access_token: token_response['access_token'],
-               smart_credentials: {
-                 access_token: token_response_body['access_token'],
-                 expires_in: token_response_body['expires_in'],
-                 token_retrieval_time:
-               }.to_json
-      end
-    end
-
-    children.unshift children.pop
   end
 end
--- lib/smart_udap_harmonization_test_kit/smart_udap_token_response_scope_test.rb
+++ lib/smart_udap_harmonization_test_kit/smart_udap_token_response_scope_test.rb
@@ -17,28 +17,39 @@ module SMART_UDAP_HarmonizationTestKit
           :udap_token_endpoint,
           :udap_client_id
 
-    output :smart_credentials,
-           :id_token,
+    input :udap_auth_code_flow_registration_scope,
+          title: 'Authorization Code Registration Requested Scope(s)',
+          description: %(
+            String containing a space delimited list of scopes requested by the client application for use in
+            subsequent requests. The Authorization Server MAY consider this list when deciding the scopes that it
+            will allow the application to subsequently request. Apps requesting the "authorization_code" grant
+            type SHOULD request user or patient scopes.
+          )
+
+    output :id_token,
            :refresh_token,
            :access_token,
            :expires_in,
            :patient_id,
            :encounter_id,
-           :received_scopes
+           :received_scopes,
+           :smart_auth_info
 
     run do
       assert_valid_json(udap_auth_code_flow_token_exchange_response_body)
       token_response_body_parsed = JSON.parse(udap_auth_code_flow_token_exchange_response_body)
 
-      output smart_credentials: {
-        refresh_token: token_response_body_parsed['refresh_token'],
-        access_token: token_response_body_parsed['access_token'],
-        expires_in: token_response_body_parsed['expires_in'],
-        client_id: udap_client_id,
-        client_secret: '',
-        udap_auth_code_flow_token_retrieval_time:,
-        token_url: udap_token_endpoint
-      }.to_json
+      smart_auth_info = Inferno::DSL::AuthInfo.new(
+        {
+          refresh_token: token_response_body_parsed['refresh_token'],
+          access_token: token_response_body_parsed['access_token'],
+          expires_in: token_response_body_parsed['expires_in'],
+          client_id: udap_client_id,
+          issue_time: udap_auth_code_flow_token_retrieval_time,
+          requested_scopes: udap_auth_code_flow_registration_scope,
+          token_url: udap_token_endpoint
+        }
+      )
 
       output id_token: token_response_body_parsed['id_token'],
              refresh_token: token_response_body_parsed['refresh_token'],
@@ -46,7 +57,8 @@ module SMART_UDAP_HarmonizationTestKit
              expires_in: token_response_body_parsed['expires_in'],
              patient_id: token_response_body_parsed['patient'],
              encounter_id: token_response_body_parsed['encounter'],
-             received_scopes: token_response_body_parsed['scope']
+             received_scopes: token_response_body_parsed['scope'],
+             smart_auth_info: smart_auth_info
 
       assert received_scopes.present?, 'Token exchange response does not include the `scope` parameter'
 
