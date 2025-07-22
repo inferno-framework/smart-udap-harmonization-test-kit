f173281dc5e6720635cc509eb6b7316d95864784
Stephen MacVicar <smacvicar@mitre.org>
2025-07-22 09:36:16 -0400

    bump version

--- lib/smart_udap_harmonization_test_kit/version.rb
+++ lib/smart_udap_harmonization_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module SMART_UDAP_HarmonizationTestKit
-  VERSION = '0.11.1'
-  LAST_UPDATED = '2025-04-04'
+  VERSION = '0.12.0'
+  LAST_UPDATED = '2025-07-22'
 end
